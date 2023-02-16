690a594e022d864ae418b3d3c08ce23831ee89e2
simoninlights <your_email@abc.example>
2023-02-17 02:50:22 +0700

    new

--- app.js
+++ app.js
@@ -2,3 +2,27 @@
 
 const year = new Date()
 document.querySelector('.current-year').innerHTML = year.getFullYear()
+
+/* Scroll to top button */
+const scrollBtn = document.querySelector('.scroll-to-top');
+
+
+const refreshBtnVisibility = () => {
+    if (document.documentElement.scrollTop <= 150) {
+        scrollBtn.style.display = "none";
+    } else {
+        scrollBtn.style.display = "block";
+    }
+};
+refreshBtnVisibility();
+window.scrollTo(0, 1000);
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+});
+
+document.addEventListener('scroll', (e) => {
+    refreshBtnVisibility();
+});
